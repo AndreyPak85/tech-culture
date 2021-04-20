c4831462d224160c32a8db414ad6c1aa928a7dc2
Andrey Pak <andrey.pak85@gmail.com>
2021-04-20 11:43:43 +0600

    cancel task :(

---
+++ canceled-tasks/task-3/script.js
@@ -0,0 +1,18 @@
+// function restoreString(s, numbers) {
+//     const newArr = [];
+//     for (let i=0; i<numbers.length; i++) {
+//         newArr.push(s[numbers.indexOf(i)])
+//     }
+//     return newArr.join('');
+// }
+
+// console.log(restoreString('codeleet', [4,5,6,7,0,2,1,3]))
+// console.log(restoreString('abc', [0,1,2]))
+// console.log(restoreString('aiohn', [3,1,4,2,0]))
+// console.log(restoreString('aaiougrt', [4,0,2,6,7,3,1,5]))
+// console.log(restoreString('art', [1, 0, 2]))
+
+
+
+let readline = require('readline');
+console.log(readline);
