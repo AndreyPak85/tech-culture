f53fefa1f42eee851076a859f801d88015ba19c4
Andrey Pak <andrey.pak85@gmail.com>
2021-04-21 08:52:19 +0600

    edited task 1

--- task-1/script.js
+++ task-1/script.js
@@ -10,13 +10,11 @@
 const num = 266219;
 const arr = num.toString().split('');
 let total = 1;
-let res = 1;
 
 for (let i=0; i<arr.length; i++) {
     total *= +arr[i]
 }
-
 console.log(total);
 
 total **=3
-console.log(`Возвести в степень 3 = ${total}`)
+console.log(`Возвести в степень 3 = ${total.toString().slice(0,2)}`)
